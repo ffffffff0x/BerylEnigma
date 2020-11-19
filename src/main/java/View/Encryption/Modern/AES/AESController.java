864682ca8e928d94c6b829f6dcca255636aa7199
RyuZU <syl18251309390@gmail.com>
2020-11-19 22:02:50 +0800

    缓慢更新AES中

--- src/main/java/View/Encryption/Modern/AES/AESController.java
+++ src/main/java/View/Encryption/Modern/AES/AESController.java
@@ -3,11 +3,21 @@ package View.Encryption.Modern.AES;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
+/**
+ * @author RyuZU
+ */
 public class AESController {
+
+    private Integer AP_OPTION_STATES = 0;
 
     @FXML private JFXButton JBT_enCode;
     @FXML private JFXButton JBT_deCode;
@@ -15,9 +25,11 @@ public class AESController {
     @FXML private JFXTextArea JTA_dst;
     @FXML private JFXComboBox JCB_charset;
     @FXML private JFXComboBox JCB_split;
+    @FXML private AnchorPane AP_option;
     @FXML private JFXButton JBT_option;
 
     @FXML private void initialize(){
+        AP_option.setVisible(false);
         initButtonOption();
     }
 
@@ -31,7 +43,48 @@ public class AESController {
 
     }
 
+    @FXML
+    public void ONClick_JBT_option(){
+        OptionPaneAnime(AP_OPTION_STATES);
+    }
+
     public void initButtonOption(){
         JBT_option.setGraphic(new ImageView(new Image(this.getClass().getResourceAsStream("/img/settings-5-fill.png"))));
+    }
+
+    public void OptionPaneAnime(Integer states){
+        Timeline timeLine = new Timeline();
+        KeyFrame kf_AP_Start;
+        KeyFrame kf_AP_Stop;
+        KeyFrame kf_BT_Start;
+        KeyFrame kf_BT_Stop;
+        if(states == 0){
+            kf_AP_Start = new KeyFrame(Duration.seconds(0), "Start", event -> AP_option.setVisible(true),
+                    new KeyValue(AP_option.layoutXProperty(), 610));
+            kf_AP_Stop = new KeyFrame(Duration.seconds(0.4), "Stop", event -> { },
+                    new KeyValue(AP_option.layoutXProperty(), 455));
+
+            kf_BT_Start = new KeyFrame(Duration.seconds(0), "Start", event -> { },
+                    new KeyValue(JBT_option.layoutXProperty(), 570));
+            kf_BT_Stop = new KeyFrame(Duration.seconds(0.4), "Stop", event -> { },
+                    new KeyValue(JBT_option.layoutXProperty(), 410));
+
+            timeLine.getKeyFrames().addAll(kf_AP_Start,kf_AP_Stop,kf_BT_Start,kf_BT_Stop);
+            AP_OPTION_STATES = 1;
+        }else{
+            kf_AP_Start = new KeyFrame(Duration.seconds(0), "Start", event -> { },
+                    new KeyValue(AP_option.layoutXProperty(), 455));
+            kf_AP_Stop = new KeyFrame(Duration.seconds(0.4), "Stop", event -> { },
+                    new KeyValue(AP_option.layoutXProperty(), 610));
+
+            kf_BT_Start = new KeyFrame(Duration.seconds(0), "Start", event -> { },
+                    new KeyValue(JBT_option.layoutXProperty(), 410));
+            kf_BT_Stop = new KeyFrame(Duration.seconds(0.4), "Stop", event -> AP_option.setVisible(false),
+                    new KeyValue(JBT_option.layoutXProperty(), 570));
+
+            timeLine.getKeyFrames().addAll(kf_AP_Start,kf_AP_Stop,kf_BT_Start,kf_BT_Stop);
+            AP_OPTION_STATES = 0;
+        }
+        timeLine.play();
     }
 }
