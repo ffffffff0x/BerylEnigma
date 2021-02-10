220108a3e62ff078f94c8b66aa027806a4cac940
RyuZU <syl18251309390@gmail.com>
2021-02-10 13:50:56 +0800

    新增Github主页按钮

--- src/main/java/View/Root/RootViewController.java
+++ src/main/java/View/Root/RootViewController.java
@@ -63,6 +63,25 @@ public class RootViewController {
     }
 
     @FXML
+    private void openURL(){
+        if (java.awt.Desktop.isDesktopSupported()) {
+            try {
+                // 创建一个URI实例
+                java.net.URI uri = java.net.URI.create("https://github.com/ffffffff0x/BerylEnigma");
+                // 获取当前系统桌面扩展
+                java.awt.Desktop dp = java.awt.Desktop.getDesktop();
+                // 判断系统桌面是否支持要执行的功能
+                if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
+                    // 获取系统默认浏览器打开链接
+                    dp.browse(uri);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
     private void test(){
         Image img = new Image("/img/icon_book-2-line.png");
         ImageView view = new ImageView(img);
