448774ce7044bd4a71172586e0e6f79c3e3fa36d
RyuZU <syl18251309390@gmail.com>
2021-04-28 15:01:22 +0800

    修改页面布局细节

--- src/main/java/ffffffff0x/beryenigma/Kit/TempView/TempViewController.java
+++ src/main/java/ffffffff0x/beryenigma/Kit/TempView/TempViewController.java
@@ -1,33 +1,7 @@
 package ffffffff0x.beryenigma.Kit.TempView;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextArea;
-import javafx.fxml.FXML;
+import ffffffff0x.beryenigma.App.View.Viewobj.ViewControllerObject;
 
-import javax.swing.*;
-
-public class TempViewController {
-
-    @FXML private JFXButton JBT_enCode;
-    @FXML private JFXButton JBT_deCode;
-    @FXML private JFXTextArea JTA_src;
-    @FXML private JFXTextArea JTA_dst;
-    @FXML private JFXComboBox JCB_charset;
-    @FXML private JTextField JTF_split;
-
-    @FXML private void initialize(){
-
-    }
-
-    @FXML
-    public void ONClick_JBT_enCode(){
-        JTA_dst.setText(JTA_src.getText());
-    }
-
-    @FXML
-    public void ONClick_JBT_deCode(){
-
-    }
+public class TempViewController extends ViewControllerObject {
 
 }
