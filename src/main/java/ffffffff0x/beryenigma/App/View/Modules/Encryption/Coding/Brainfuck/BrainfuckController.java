906ee46bf584d25573c4aa0dae3766ece9670319
RyuZU <syl18251309390@gmail.com>
2021-11-23 21:48:40 +0800

    增强 brainfuck解密时增加进度条防止UI主线程卡死

--- src/main/java/ffffffff0x/beryenigma/App/View/Modules/Encryption/Coding/Brainfuck/BrainfuckController.java
+++ src/main/java/ffffffff0x/beryenigma/App/View/Modules/Encryption/Coding/Brainfuck/BrainfuckController.java
@@ -1,10 +1,12 @@
 package ffffffff0x.beryenigma.App.View.Modules.Encryption.Coding.Brainfuck;
 
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXSpinner;
 import ffffffff0x.beryenigma.App.Controller.Encryption.Coding.Brainfuck.Coding_Brainfuck;
 import ffffffff0x.beryenigma.App.View.Viewobj.ViewController;
 import ffffffff0x.beryenigma.Init.ViewInit;
 import ffffffff0x.beryenigma.Kit.Utils.ViewUtils;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 /**
@@ -16,6 +18,8 @@ public class BrainfuckController extends ViewController {
     @FXML private JFXComboBox<String> JCB_modeCheck;
     @FXML private JFXComboBox<String> JCB_charset;
 
+    @FXML
+    private JFXSpinner JSP_running;
     /**
      * 全局界面初始化
      */
@@ -31,17 +35,26 @@ public class BrainfuckController extends ViewController {
     @Override
     public void ONClickDecode() {
         super.ONClickDecode();
-        try {
-            if (JCB_modeCheck.getValue().equals("Brainfuck")) {
-                JTA_dst.setText(Coding_Brainfuck.BrainfuckDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue()))));
-            }else if (JCB_modeCheck.getValue().equals("Ook")) {
-                JTA_dst.setText(Coding_Brainfuck.OokDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue()))));
-            }else if (JCB_modeCheck.getValue().equals("Trollscript")) {
-                JTA_dst.setText(Coding_Brainfuck.TrollscriptDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue()))));
-            }
-        }catch (Exception e) {
-            ViewUtils.textAreaValidate(JTA_dst);
-        }
+            JSP_running.setVisible(true);
+            new Thread(() -> {
+                String result = "";
+                try {
+                    if (JCB_modeCheck.getValue().equals("Brainfuck")) {
+                        result = Coding_Brainfuck.BrainfuckDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue())));
+                    }else if (JCB_modeCheck.getValue().equals("Ook")) {
+                        result = Coding_Brainfuck.OokDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue())));
+                    }else if (JCB_modeCheck.getValue().equals("Trollscript")) {
+                        result = Coding_Brainfuck.TrollscriptDeCode(new String(JTA_src.getText().getBytes(JCB_charset.getValue())));
+                    }
+                }catch (Exception e) {
+                    ViewUtils.textAreaValidate(JTA_dst);
+                }
+                String finalResult = result;
+                Platform.runLater(() -> {
+                    JTA_dst.setText(finalResult);
+                    JSP_running.setVisible(false);
+                });
+            }).start();
     }
 
     private void ComboBoxInit() {
