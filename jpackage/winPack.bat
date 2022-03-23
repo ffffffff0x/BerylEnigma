3ab1d5dea9748182f9176a29102bc36423cb0ad2
RyuZU <16266917+ryuzusunc@users.noreply.github.com>
2022-03-23 22:37:26 +0800

    更新windows，linux端打包方式，优化pom文件

---
+++ jpackage/winPack.bat
@@ -0,0 +1,8 @@
+set /p var1=Input Program Version:
+cd ../
+call mvn clean
+call mvn javafx:jlink
+cd ./jpackage
+jpackage -n BE-BerylEnigma --app-version %var1% -m "beryenigma/ffffffff0x.beryenigma.Main" --runtime-image ..\target\beryenigma --dest ..\target\ --java-options "-Djava.class.path=.\runtime\ext\bcprov-jdk15on-1.70.jar --add-opens=java.base/java.lang.reflect=com.jfoenix --add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=com.jfoenix --add-exports=javafx.base/com.sun.javafx.binding=com.jfoenix --add-exports=javafx.base/com.sun.javafx.event=com.jfoenix --add-exports=javafx.graphics/com.sun.javafx.stage=com.jfoenix --add-exports=javafx.graphics/com.sun.javafx.scene=com.jfoenix --add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=com.jfoenix --add-exports=javafx.controls/com.sun.javafx.scene.control=com.jfoenix" --icon .\ico\BerylEnigma.ico --type app-image --description "BerylEnigma" --vendor "RyuZU,ffffffff0x" --copyright "Copyright 2022 ffffffff0x,MIT License"
+xcopy .\conf\java.security ..\target\BE-BerylEnigma\runtime\conf\security\ /y
+xcopy .\ext ..\target\BE-BerylEnigma\runtime\ext\
