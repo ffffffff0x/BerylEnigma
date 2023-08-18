06362f43b6d7f9397c1a75066f6d5a950a052fb6
RyuZU <16266917+ryuzusunc@users.noreply.github.com>
2023-08-18 17:25:52 +0800

    文件头检查器UI功能完善中

--- src/main/java/ffffffff0x/beryenigma/App/View/Modules/Tools/RedTeam/FileHeadChecker/FileHeadCheckerController.java
+++ src/main/java/ffffffff0x/beryenigma/App/View/Modules/Tools/RedTeam/FileHeadChecker/FileHeadCheckerController.java
@@ -7,6 +7,7 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
 import ffffffff0x.beryenigma.App.View.Viewobj.ViewController;
+import ffffffff0x.beryenigma.Init.ImageListInit;
 import ffffffff0x.beryenigma.Kit.Utils.ViewNode;
 import ffffffff0x.beryenigma.Kit.Utils.ViewUtils;
 import javafx.animation.KeyFrame;
@@ -14,6 +15,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
@@ -33,20 +35,28 @@ import java.util.stream.Collectors;
  **/
 @ViewNode(name = "FileHeadChecker",folderPath = "Root/Tools/RedTeam/",fxmlName = "FileHeadCheckerView.fxml")
 public class FileHeadCheckerController extends ViewController {
+    // 切换/加载 文件头信息JSON控件
     @FXML
     JFXToggleButton JTB_ModeSwitch;
+    // 显示已经加载的文件头信息JSON的名称 默认为 UseDefaultHeaderJSON
     @FXML
     Label JLB_JSONName;
+    // 显示JSON中文件头信息的数量
     @FXML
     Label JLB_JSONNum;
+    // 显示已经加载的问价的数量
     @FXML
     Label JLB_FileNum;
+    // 检测文件头十六进制数的数量 默认为4
     @FXML
     JFXTextField JTF_CheckHexNum;
+    // 检测按钮 按下开始检测
     @FXML
     JFXButton JBT_Check;
+    // 文件加载按钮 使用多选文件 不可选择文件夹
     @FXML
     JFXButton JBT_LoadFiles;
+    // 显示所有已经加载文件的全路径
     @FXML
     JFXTextArea JTA_FileList;
 
@@ -62,6 +72,10 @@ public class FileHeadCheckerController extends ViewController {
     // JSON序列化工具
     Gson gson = new Gson();
 
+    // 加载文件按钮的图片
+    final ImageView IMG_FileAdd = new ImageView(ViewUtils.getImage(ImageListInit.ICON_JBT_FILEADD));
+    final ImageView IMG_FileAdd_M = new ImageView(ViewUtils.getImage(ImageListInit.ICON_MJBT_FILEADD));
+
     @Override
     protected void initialize() {
         // 初始化本地文件头信息
@@ -72,19 +86,30 @@ public class FileHeadCheckerController extends ViewController {
         Type type = new TypeToken<HashMap<String, FileHeaderBean>>(){}.getType();
         fileTypes = gson.fromJson(jsonData, type);
 
+        // 显示默认文件头JSON文件中 文件头信息的数量
         JLB_JSONNum.setText("Num: " + fileTypes.size());
+
+        fileLoadButtonLoadImage(JBT_LoadFiles);
     }
 
     @FXML
     public void ONClickLoadFiles() {
         zoomButton(JBT_LoadFiles);
+        fileLoadButtonLoadSImage(JBT_LoadFiles);
+        JBT_LoadFiles.setOpacity(0.8);
+        LoadFilePaths();
+    }
+
+    public void LoadFilePaths() {
         files = ViewUtils.getFiles();
         StringJoiner stringJoiner = new StringJoiner("\n");
-        for (File file : files) {
-            stringJoiner.add(file.getPath());
+        if (files != null) {
+            for (File file : files) {
+                stringJoiner.add(file.getPath());
+            }
+            JTA_FileList.setText(stringJoiner.toString());
+            JLB_FileNum.setText("FileNum: " + files.size());
         }
-        JTA_FileList.setText(stringJoiner.toString());
-        JLB_FileNum.setText("FileNum: " + files.size());
     }
 
     public void zoomButton(JFXButton jfxButton) {
@@ -98,6 +123,20 @@ public class FileHeadCheckerController extends ViewController {
             AnchorPane.setRightAnchor(jfxButton, 20.0);
             AnchorPane.setBottomAnchor(jfxButton, 5.0);
         }
+    }
+
+    public void fileLoadButtonLoadImage(JFXButton jfxButton) {
+        IMG_FileAdd.setFitHeight(290);
+        IMG_FileAdd.setFitWidth(220);
+        IMG_FileAdd.setPreserveRatio(true);
+        jfxButton.setGraphic(IMG_FileAdd);
+    }
+
+    public void fileLoadButtonLoadSImage(JFXButton jfxButton) {
+        IMG_FileAdd_M.setFitHeight(32);
+        IMG_FileAdd_M.setFitWidth(32);
+        IMG_FileAdd_M.setPreserveRatio(true);
+        jfxButton.setGraphic(IMG_FileAdd_M);
     }
 
     public void buttonAnimate() {
---
+++ src/main/java/ffffffff0x/beryenigma/App/View/Modules/Tools/RedTeam/FileHeadChecker/FileHeadCheckerImpl.java
@@ -0,0 +1,103 @@
+package ffffffff0x.beryenigma.App.View.Modules.Tools.RedTeam.FileHeadChecker;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+/**
+ * @program: BerylEnigma
+ * @author: RyuZU
+ * @create: 2023-08-14 15:30
+ **/
+
+public class FileHeadCheckerImpl {
+    // 获取文件类型
+    public static FileHeaderBean getFileType(String filePath,HashMap<String,FileHeaderBean> fileTypes) {
+        FileHeaderBean fileHeaderBean = fileTypes.get(getFileHeader(filePath));
+        if (fileHeaderBean != null) {
+            return fileHeaderBean;
+        }else {
+            return new FileHeaderBean("UNKNOWN",getFileHeader(filePath),"NULL");
+        }
+    }
+
+    // 根据文件路径获取文件头信息
+    public static String getFileHeader(String filePath) {
+        FileInputStream is = null;
+        String value = null;
+        try {
+            is = new FileInputStream(filePath);
+            byte[] b = new byte[4];
+            /*
+             * int read() 从此输入流中读取一个数据字节。int read(byte[] b) 从此输入流中将最多 b.length
+             * 个字节的数据读入一个 byte 数组中。 int read(byte[] b, int off, int len)
+             * 从此输入流中将最多 len 个字节的数据读入一个 byte 数组中。
+             */
+            is.read(b, 0, b.length);
+            value = bytesToHexString(b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return value;
+    }
+
+    // 根据文件流获取文件头信息
+    public static String getFileHeaderByFileInputStream(FileInputStream is) {
+        String value = null;
+        try {
+            byte[] b = new byte[4];
+            /*
+             * int read() 从此输入流中读取一个数据字节。int read(byte[] b) 从此输入流中将最多 b.length
+             * 个字节的数据读入一个 byte 数组中。 int read(byte[] b, int off, int len)
+             * 从此输入流中将最多 len 个字节的数据读入一个 byte 数组中。
+             */
+            is.read(b, 0, b.length);
+            value = bytesToHexString(b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return value;
+    }
+
+    // 将要读取文件头信息的文件的byte数组转换成string类型表示
+    private static String bytesToHexString(byte[] src) {
+        StringBuilder builder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+        String hv;
+        for (int i = 0; i < src.length; i++) {
+            // 以十六进制（基数 16）无符号整数形式返回一个整数参数的字符串表示形式，并转换为大写
+            hv = Integer.toHexString(src[i] & 0xFF).toUpperCase();
+            if (hv.length() < 2) {
+                builder.append(0);
+            }
+            builder.append(hv);
+        }
+        //System.out.println(builder.toString());
+        return builder.toString();
+    }
+
+
+}
